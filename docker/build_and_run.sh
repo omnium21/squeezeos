1aa7a1ca0f06a55f7c61b95205f1cf6d7dce533f
Nicolas Aguirre <aguirre.nicolas@gmail.com>
2020-04-16 14:37:00 +0200

    Logged in as user

--- docker/build_and_run.sh
+++ docker/build_and_run.sh
@@ -6,11 +6,10 @@ echo "Building squeezeos image builder..."
 docker build -t squeezeos_builder .
 if docker ps -a | grep -q squeezeos_builder
 then
-	echo "Deleting previous version of squeezeos builder container..."
+	echo "Stopping previous version of squeezeos builder container..."
 	docker stop squeezeos_builder
-	docker rm -f squeezeos_builder
 fi
 
 echo "Starting squeezeos image builder..."
+docker run -it --rm  -v ${PWD}/../poky:/home/squeezeos/poky squeezeos_builder
 
-docker run -it --name squeezeos_builder -v ${PWD}/../poky:/home/squeezeos/poky squeezeos_builder /bin/bash
